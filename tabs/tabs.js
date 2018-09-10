7d01f0157d68b5e5a4aa3593c6dd05f3441cc18e
Archana <archana462007@gmail.com>
2018-09-10 15:12:17 +0400

    Jquery Tabs

---
+++ tabs/tabs.js
@@ -0,0 +1,12 @@
+$(document).ready(function(e) {   
+     $(".tab-list").on("click", ".tab", function(e) {
+    e.preventDefault();
+
+    $(".tab").removeClass("active");
+    $(".tab-content").removeClass("show");
+    $(this).addClass("active");
+    $($(this).attr("href")).addClass("show");
+	});
+ });
+
+
